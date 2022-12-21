7c3552019d8615e0f1adcb4aefabe04389ac4f17
Sidnym Ladrut <sidnym@ladrut.xyz>
2022-12-21 02:33:34 +0000

    dramatically improve /key page (formatting, error checking, etc.)

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,17 +1,18 @@
 import React, { useState, useContext } from 'react';
-import { redirect, useLoaderData, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  redirect, useLoaderData,
+  createBrowserRouter, RouterProvider
+} from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { PlusIcon, ExclamationTriangleIcon } from '@heroicons/react/24/solid'
 import api from './api';
 
 export function App() {
   const [apiData, setApiData] = useState({key: "", isSet: false});
 
   const KeyForm = () => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-
-      const formData = new FormData(event.currentTarget);
-      const key = formData.get("key");
-
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const onSubmit = ({key}) => {
       api.poke<any>({
         app: "pantheon-agent",
         mark: "pantheon-action",
@@ -21,14 +22,39 @@ export function App() {
       );
     };
 
+    const keyRegex: RegExp = /^SLA([a-f0-9]{8})-([a-f0-9]{4})-([a-f0-9]{4})-([a-f0-9]{4})-([a-f0-9]{12})TE$/;
+
     return (
       <React.Fragment>
-        <h1 className="text-3xl">Please enter your Slate API key:</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            <input className="border border-black border-solid" name="key" type="text" />
-          </label>
-          <button type="submit">Submit</button>
+        <div className="grid gap-y-8 py-16 text-center">
+          <h1>Welcome to <code>%pantheon</code>!</h1>
+          <h1>First, Sign Up for <a href="https://slate.host/_/auth?tab=signup">Slate</a>.</h1>
+          <h1>Then, Add Your <a href="https://slate.host/_/api">API Key</a>:</h1>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-row justify-center">
+            <input type="text" autocomplete="off"
+              placeholder="API Key, e.g.: SLAabcdef12-1234-abcd-1234-abcdef123456TE"
+              {...register("key", {required: true, pattern: keyRegex})}
+              className={`w-9/12 py-2 px-3 bg-bgp1
+                border border-bgp2 rounded-l-lg
+                ring-bgs2 ring-inset focus:outline-none focus:ring-2`} />
+            <button type="submit" className={`rounded-r-lg py-2 px-3
+                font-semibold border-2 transition-colors
+                text-bgp1 bg-bgs1 border-bgs2/0 hover:border-bgs2/60`}>
+              <PlusIcon className="h-6 w-6 text-fgp1" />
+            </button>
+          </div>
+          <div className="flex flex-row justify-center">
+            {errors.key &&
+              <React.Fragment>
+                <ExclamationTriangleIcon className="h-6 w-6 text-fgs1" />
+                {(errors.key.type === "required") ?
+                  (<p>Please enter your API key.</p>) :
+                  (<p>Given API key is malformatted.</p>)}
+              </React.Fragment>
+            }
+          </div>
         </form>
       </React.Fragment>
     );
